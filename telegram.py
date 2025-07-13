df7d8fe03a1a799907f89ae28a116ced7a7e51c0
EvolveMV <myronchukvalerii@gmail.com>
2025-07-13 02:14:07 -0600

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -2,9 +2,13 @@ import os
 import requests
 
 TOKEN = os.getenv("TELEGRAM_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
+CHAT_ID = os.getenv("USER_CHAT_ID")
 
 def send_message(text):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
     payload = {"chat_id": CHAT_ID, "text": text}
-    requests.post(url, data=payload)
+    try:
+        response = requests.post(url, data=payload)
+        print("Response:", response.text)
+    except Exception as e:
+        print("Error sending message:", e)
