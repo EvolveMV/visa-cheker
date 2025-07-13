12312ec9fb1f4887eec3db14fdd36300e75436df
EvolveMV <myronchukvalerii@gmail.com>
2025-07-13 02:23:19 -0600

    Delete variables.py

--- variables.py
+++
@@ -1,5 +0,0 @@
-headers = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
-    "X-CSRF-Token": "OBDo2mubGsMIZt3AEd52M49MJc7kBiH2EZT+Ii+/Hf+AYsabIVA2tmqceTO3VtekWoFZ2yyBvZ79MLV7BngMmA==",
-    "cookie": "_gid=GA1.2.965899369.1752390237; _ga_CSLL4ZEK4L=GS2.1.s1752390237$o1$g1$t1752391248$j16$l0$h0; _ga=GA1.2.1221412463.1752390237; _ga_W1JNKHTW0Y=GS2.2.s1752390237$o1$g1$t1752391255$j9$l0$h0; _yatri_session=RiY134..."
-}
